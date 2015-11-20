7db42418c609fcdddd71959ffe03e67664da554c
Jovanny Cruz <jovannypcg@yahoo.com>
2015-11-20 11:40:35 -0600

    First commit

---
+++ src/main/java/mx/jovannypcg/linkedlist/App.java
@@ -0,0 +1,13 @@
+package mx.jovannypcg.linkedlist;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
